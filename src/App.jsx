05403da7de3d7c86f75cef1927203b449c243cb0
EC2 Default User <ec2-user@ip-172-31-16-168.us-west-2.compute.internal>
2026-01-30 16:13:26 +0000

    fixing footer

--- src/App.jsx
+++ src/App.jsx
@@ -24,40 +24,45 @@ function App() {
           <Route path="/locus/:name" element={<LocusPage />} />
           <Route path="/reference/:id" element={<ReferencePage />} />
         </Routes>
-
         <footer className="main-footer">
+          {/* Top navigation strip */}
           <div className="footer-nav">
             <div className="footer-left">
               <Link to="/">
                 <span className="arrow-up">↑</span> Return to CGD
               </Link>
             </div>
-            <div className="footer-right">
-              <a href="/cgi-bin/suggestion">
-                Send a Message to the CGD Curators <span className="email-icon">✉</span>
-              </a>
-            </div>
+          <div className="footer-right">
+            <a href="/cgi-bin/suggestion">
+              Send a Message to the CGD Curators <span className="email-icon">✉</span>
+            </a>
           </div>
-          <div className="footer-copyright">
-            <p>
-              <strong>CGD Copyright</strong> &copy; 2004–{new Date().getFullYear()} The Board of Trustees, Leland Stanford Junior University.
-            </p>
-            <p>
-              Permission to use the information contained in this database was given by the researchers/institutes
-              who contributed or published the information. Users of the database are solely responsible for compliance
-              with any copyright restrictions, including those applying to the author abstracts. Documents from this
-              server are provided "AS-IS" without any warranty, expressed or implied.
-            </p>
-            <p className="citation-text">
-              <span className="cite-label">To cite CGD</span>, please use the following reference:{' '}
-              Skrzypek MS, Binkley J, Binkley G, Miyasato SR, Simison M, Sherlock G (2017).{' '}
-              The Candida Genome Database (CGD): incorporation of Assembly 22, systematic identifiers
-              and visualization of high throughput sequencing data.{' '}
-              <em>Nucleic Acids Res</em> <strong>45</strong> (D1); D592-D596;{' '}
-              see <a href="/HowToCite.shtml">How to cite CGD</a>.
-            </p>
-          </div>
-        </footer>
+        </div>
+
+        {/* Copyright block */}
+        <div className="footer-copyright">
+          <p>
+            <strong>CGD Copyright</strong> © 2004–{new Date().getFullYear()} The Board of Trustees, Leland Stanford Junior University.
+          </p>
+
+          <p>
+            Permission to use the information contained in this database was given by the
+            researchers/institutes who contributed or published the information. Users of the
+            database are solely responsible for compliance with any copyright restrictions,
+            including those applying to the author abstracts. Documents from this server are
+            provided "AS-IS" without any warranty, expressed or implied.
+          </p>
+
+          <p className="citation-text">
+            <span className="cite-label">To cite CGD</span>, please use the following reference:{' '}
+            Skrzypek MS, Binkley J, Binkley G, Miyasato SR, Simison M, Sherlock G (2017).
+            The Candida Genome Database (CGD): incorporation of Assembly 22, systematic identifiers
+            and visualization of high throughput sequencing data.{' '}
+            <em>Nucleic Acids Res</em> <strong>45</strong> (D1); D592–D596; see{' '}
+            <a href="/HowToCite.shtml">How to cite CGD</a>.
+          </p>
+        </div>
+      </footer>	  	  
       </div>
     </Router>
   );
